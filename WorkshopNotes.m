7da93accd7b16f24df922c9a679284d76cb3dfa7
lcraemer <lcraemer@gmx.de>
2025-02-25 11:56:13 +0100

    Reapply "Create WorkshopNotes.m"
    This reverts commit 3338aeb59a8b2a226c8fd1aeeaa6404840e8bbfc.

---
+++ WorkshopNotes.m
@@ -0,0 +1,40 @@
+%% 2nd Modelling Workshop
+% 
+% % Day 1
+% 
+% - Model agnostic means with very few assumptions
+% - How precise are estimates?
+% - compare the descriptive data to the bayesian model. Check for
+% similarity of the two datasets
+% - 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
+% 
